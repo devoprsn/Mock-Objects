49438841fcc10152cbc6407a7d60150064458873
devoprsn <devoprsn@gmail.com>
2017-10-27 02:48:23 -0400

    Updated Gambling.java

--- src/Gambling.java
+++ src/Gambling.java
@@ -1,4 +1,36 @@
 
 public class Gambling {
-
+	 int balance;
+	 int minBalance;
+	 IRandomValueGenerator rand;
+  
+	 public Gambling(int minBalance, IRandomValueGenerator rand)
+	 {
+	    if(minBalance<0)
+	    	throw new IllegalArgumentException();
+	    
+	    this.minBalance=minBalance;
+	    balance=0;
+	    this.rand=rand;
+	 }
+	  
+	 public double getCurrentBalance()
+	 {
+	    return balance;
+	 }
+	  
+	 public void addMoney(int amount)
+	 {
+	    
+	 }
+	  
+	 public void betOnANumber(int amnt, int min, int max, int selectedNumber)
+	 {
+	    
+	 }
+	  
+	 public void betOnProbability(int amnt, double p)
+	 {
+	    
+	 }
 }
