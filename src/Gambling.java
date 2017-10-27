2204c029522239d8830a852e59da05a0c09c85e0
devoprsn <devoprsn@gmail.com>
2017-10-26 21:02:45 -0400

    Update Gambling.java

--- src/Gambling.java
+++ src/Gambling.java
@@ -1,4 +1,30 @@
 
 public class Gambling {
-
+  int balance;
+  IRandomValueGenerator rand;
+  
+  public Gambling(int minBalance, IRandomValueGenerator rand)
+  {
+    
+  }
+  
+  public double getCurrentBalance()
+  {
+    
+  }
+  
+  public void addMoney(int amount)
+  {
+    
+  }
+  
+  public void betOnANumber(int amnt, int min, int max, int selectedNumber)
+  {
+    
+  }
+  
+  public void betOnProbability(int amnt, double p)
+  {
+    
+  }
 }
